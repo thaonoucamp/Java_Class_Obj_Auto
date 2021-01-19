89b11a599c646b9c18ba879f243fcd0ed4961de4
thaonoucamp <dangxuanthaoht@gamil.com>
2021-01-19 16:15:34 +0700

    add

---
+++ src/myPackage/Movie.java
@@ -0,0 +1,10 @@
+package myPackage;
+
+public class Movie {
+    public static void main(String[] args) {
+        Movie mv = new Movie();
+
+        Object obj = new Movie();
+    }
+}
+// 7_ Kieu mac dinh moi kieu la Obj ma ko can Extends;
